64392d9879c2e9ee4ae53d0f78cacf94581741e0
Martin Szlapa <szlapamartin@gmail.com>
2024-05-02 17:06:53 -0400

    Llama installed. The project pings llama to analyse tweets but is very laggy as of yet.

---
+++ node-backend/server/combined.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+const { Rettiwt } = require("rettiwt-api");
+
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(cors());
+
+const rettiwtInstance = new Rettiwt({apiKey: process.env.RETTIWT_API_KEY});
+const { default: ollama} = require("ollama");
+
+const prompt = "Put the following Tweet into one of the three following categories: 'politics'," +
+    " 'humour', 'technology', or 'other'. Please ONLY return one (1) of these four words, with ABSOLUTELY NO other text preceding or following." +
+    " Input follows this sentence and will be of the format 'username: <username>, content: <tweet content>', where username is the Twitter username of the author, and content is the content of the tweet."
+
+app.get('/api/analyse', async (req, res) => {
+    const {user, count} = req.query;
+    console.log(`getting ${count} tweets`);
+
+    try {
+        const tweets = await rettiwtInstance.tweet.search({
+            fromUsers: [user],
+            count: count
+        });
+
+        const categorisedTweets = await Promise.all(tweets.list.map(async tweet => {
+            console.log(tweet.fullText);
+            const category = await ollama.chat({
+                model: 'llama3',
+                messages: [{role: 'user', content: `username: ${tweet.tweetBy.fullName} content: ${tweet.fullText}`}]
+            });
+            return {
+                fullText: tweet.fullText,
+                category: category
+            };
+        }));
+
+        res.json(categorisedTweets);
+    }
+    catch (error){
+        console.error(error);
+        res.status(500).json({message: 'Tweet or llama error'});
+    }
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
--- src/AnalysisChart/AnalysisChart.js
+++ src/AnalysisChart/AnalysisChart.js
@@ -1,2 +1,21 @@
 import React from 'react';
 
+const AnalysisChart = ( {tweets} ) => {
+    if (!tweets.list) return;
+    // For now, let's say that the relevant information of a tweet as pertaining to its category is:
+    // username
+    // image
+    // tweet contents
+    const mappedTweets = tweets.list.map(tweet => ({
+        fullName: tweet.tweetBy.fullName,
+        tweetImage: tweet.media, // an array of objects with 'url' and 'type' (e.g. type = photo, url = https://pbs.twimg.com/media/...)
+        fullText: tweet.fullText
+    }));
+    return(
+      <div>
+
+      </div>
+    );
+}
+
+export default AnalysisChart;
